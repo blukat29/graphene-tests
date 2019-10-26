a9b07bb44610740bf195eb111874c9886dc4b1d9
Yunjong Jeong <yunjong@kaist.ac.kr>
2019-10-27 06:50:27 +0900

    nodejs helloworld

---
+++ nodejs/scripts/lite_server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var process = require('process');
+
+var port = process.argv[2] || '9000';
+console.log('Using port: ' + port);
+port = parseInt(port);
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type' : 'text/plain'});
+  response.write('hello\n');
+  response.end();
+}).listen(port);
